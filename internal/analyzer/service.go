060f6e88e524df7b6084b6a2b476b6d2e7e3981a
Mert Çıkla <mertcikla@gmail.com>
2026-09-14 09:57:47 +0300

    refactor: remove internal symbol package

--- internal/analyzer/service.go
+++ internal/analyzer/service.go
@@ -3,10 +3,8 @@ package analyzer
 import (
 	"context"
 	"errors"
-	"os"
 
 	"github.com/mertcikla/tld/v2/internal/ignore"
-	"github.com/mertcikla/tld/v2/internal/symbol"
 )
 
 type Service interface {
@@ -14,13 +12,7 @@ type Service interface {
 	HasSymbol(ctx context.Context, filePath, symbolName string) (bool, error)
 }
 
-type LegacyService struct{}
-
 var defaultService Service = NewService()
-
-func NewLegacyService() *LegacyService {
-	return &LegacyService{}
-}
 
 func DefaultService() Service {
 	return defaultService
@@ -32,38 +24,5 @@ func HasSymbol(ctx context.Context, filePath, symbolName string) (bool, error) {
 
 func IsUnsupportedLanguage(err error) bool {
 	var analyzerUnsupported ErrUnsupportedLanguage
-	if errors.As(err, &analyzerUnsupported) {
-		return true
-	}
-	var unsupported symbol.ErrUnsupportedLanguage
-	return errors.As(err, &unsupported)
-}
-
-func (s *LegacyService) ExtractPath(ctx context.Context, path string, rules *ignore.Rules, onEntry func(path string, isDir bool)) (*Result, error) {
-	result, err := legacyExtractPath(ctx, path, rules, onEntry)
-	if err != nil {
-		return nil, err
-	}
-	return resultFromLegacy(result), nil
-}
-
-func (s *LegacyService) HasSymbol(ctx context.Context, filePath, symbolName string) (bool, error) {
-	return symbol.HasSymbol(ctx, filePath, symbolName)
-}
-
-func legacyExtractPath(ctx context.Context, path string, rules *ignore.Rules, onEntry func(path string, isDir bool)) (*symbol.Result, error) {
-	info, err := os.Stat(path)
-	if err != nil {
-		return nil, err
-	}
-	if info.IsDir() {
-		return symbol.ExtractDirWithProgress(ctx, path, rules, onEntry)
-	}
-	if rules.ShouldIgnoreFile(path) {
-		return &symbol.Result{}, nil
-	}
-	if onEntry != nil {
-		onEntry(path, false)
-	}
-	return symbol.ExtractFile(ctx, path)
+	return errors.As(err, &analyzerUnsupported)
 }
